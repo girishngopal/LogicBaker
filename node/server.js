82434e5f0b4fc8d16733431787bd225ecf5616b7
VishnuBose <vishnu.bose95@gmail.com>
2017-03-01 10:00:04 +0530

    with sub
    substraction

--- node/server.js
+++ node/server.js
@@ -21,6 +21,13 @@ app.get('/add', function(req, res){
     res.send(ans+"");
         })
 
+app.get('/sub', function(req, res){
+    
+    var ans=parseInt(req.query.num1)-parseInt(req.query.num2);
+    console.log(req.query.num1,req.query.num2,ans);
+    res.send(ans+"");
+        })
+
 var port=process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
